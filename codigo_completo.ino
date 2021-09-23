d155fd1f46f2e521896969057f2fa8d83fb6d81d
LumaKido <89153095+LumaKido@users.noreply.github.com>
2021-09-23 20:05:53 +0300

    Update codigo_completo.ino

--- codigo_completo.ino
+++ codigo_completo.ino
@@ -1,19 +1,17 @@
 //HRSR04 sensor
-#define HRSR04_trig_pin 7
-#define HRSR04_echo_pin 6
+const byte HRSR04_trig_pin = 7;
+const byte HRSR04_echo_pin = 6;
 float distancy;
 
-//Motor DC
-int IN1 = 5; //roxo
-int IN2 = 4; //cinza
-int IN3 = 3; //azul
-int IN4 = 2; //amarelo
+//TCRT5000 sensor
+int pinoLed = 12;
+int pinoSensor = 8;
 
 //Definição dos pinos de controle do motor
-#define IN2 4 // Pino_Velocidade 1º Motor ( 0 a 255)_ Porta ATV_A ponte H;
-#define IN4 2 //Pino_Velocidade 2º Motor ( 0 a 255) _ Porta ATV_B ponte H;
-#define IN1 5 //Pino_Direção do 1º Motor: Para frente / Para trás (HIGH ou LOW)_ porta IN1 ponte H;
-#define IN3 3 //Pino_Direção do 2º Motor: Para frente / Para trás (HIGH ou LOW)_ porta IN3 ponte H;
+#define IN1 2 // Pino_Velocidade 1º Motor ( 0 a 255)_ Porta ATV_A ponte H;
+#define IN2 3 //Pino_Velocidade 2º Motor ( 0 a 255) _ Porta ATV_B ponte H;
+#define IN3 4 //Pino_Direção do 1º Motor: Para frente / Para trás (HIGH ou LOW)_ porta IN1 ponte H;
+#define IN4 5 //Pino_Direção do 2º Motor: Para frente / Para trás (HIGH ou LOW)_ porta IN3 ponte H;
 
 //Definição dos pinos dos sensores
 #define pin_S1 9
@@ -22,8 +20,7 @@ bool Sensor1 = 0;
 bool Sensor2 = 0;
 
 //variável responsável por controlar a velocidade dos motores
-int velocidade = 150;
-
+#define velocidade 200   
 void setup()
 {
   //sensor HCSR-04
@@ -42,8 +39,10 @@ void setup()
   digitalWrite(pinoLed, LOW);
 
   //Setamos a direção inicial do motor como 0, isso fará com que ambos os motores girem para frente
-  digitalWrite(IN1, LOW);
-  digitalWrite(IN3, LOW);
+  digitalWrite(IN1, 255);
+  digitalWrite(IN2, 255);
+  digitalWrite(IN3, 255);
+  digitalWrite(IN4, 255);
 
   //Setamos os pinos dos sensores como entrada
   pinMode(pin_S1, INPUT);
@@ -52,37 +51,22 @@ void setup()
   Serial.begin(9600);
 }
 
-void loop()
-{
+void loop() {
   distancy = getUltrassonicDistance();
-
-  //Motor DC
-  //motor A = preto e branco
-  //motor B = verde e cinza
-
   //Neste processo armazenamos o valor lido pelo sensor na variável que armazena tais dados.
-  Sensor1 = hasLight(pin_S1);
-  Sensor2 = hasLight(pin_S2);
+  Sensor1 = digitalRead(pin_S1);
+  Sensor2 = digitalRead(pin_S2);
 
-  Serial.print("Distancia em cm: ");
-  Serial.println(distancy);
-  Serial.print("sensor esquerdo ligado? - ");
-  Serial.println(Sensor1);
-  Serial.print("sensor direito ligado? - ");
-  Serial.println(Sensor2);
-
-  delay(200);
-
-  if (distancy <= 20)
+  if (distancy <= 5)
   { // Se a distância lida pelo sensor for menor ou igual que 20 centimetros
 
     // Motor lado esquerdo parado
-    digitalWrite(IN1, LOW);
-    digitalWrite(IN2, LOW);
+    analogWrite(IN1, 255);
+    analogWrite(IN2, 255);
 
     // Motor lado direito parado
-    digitalWrite(IN3, LOW);
-    digitalWrite(IN4, LOW);
+    analogWrite(IN3, 255);
+    analogWrite(IN4, 255);
   }
   else
   { // Se não, ou seja, se a distância for maior que 20 centimetros
@@ -90,26 +74,31 @@ void loop()
     if ((Sensor1 == 0) && (Sensor2 == 0))
     { // Se detectar na extremidade das faixas duas cores brancas
 
-      analogWrite(IN2, velocidade); // Ambos motores ligam na mesma velocidade
-      analogWrite(IN4, velocidade);
+        analogWrite(IN1, velocidade); // Ambos motores ligam na mesma velocidade
+        analogWrite(IN2, velocidade);
+        analogWrite(IN3, velocidade); 
+        analogWrite(IN4, velocidade);
     }
     if ((Sensor1 == 1) && (Sensor2 == 0))
     { // Se detectar um lado preto e o outro branco
 
-      analogWrite(IN2, 0);          // O motor 1 desliga
-      analogWrite(IN4, velocidade); // O motor 2 fica ligado, fazendo assim o carrinho virar
+      analogWrite(IN4,255 ); // O motor 1 desliga
+      analogWrite(IN3, 255); 
+      analogWrite(IN2, velocidade); // O motor 2 fica ligado, fazendo assim o carrinho virar
+      analogWrite(IN1, velocidade); 
     }
     if ((Sensor1 == 0) && (Sensor2 == 1))
     { // Se detectar um lado branco e o outro preto
 
-      analogWrite(IN2, velocidade); //O motor 1 fica ligado
-      analogWrite(IN4, 0);          // O motor 2 desliga, fazendo assim o carrinho virar no outro sentido
+      analogWrite(IN4, velocidade); //O motor 1 fica ligado
+      analogWrite(IN3, velocidade);
+      analogWrite(IN2, 255);
+      analogWrite(IN1, 255);          // O motor 2 desliga, fazendo assim o carrinho virar no outro sentido
     }
   }
 }
 
-float getUltrassonicDistance()
-{
+float getUltrassonicDistance() {
   digitalWrite(HRSR04_trig_pin, LOW);
   delayMicroseconds(2);
   digitalWrite(HRSR04_trig_pin, HIGH);
@@ -122,7 +111,6 @@ float getUltrassonicDistance()
   return distancyInCentimeters;
 }
 
-bool hasLight(byte pin)
-{
-  return (digitalRead(pin) == 0);
+bool hasLight() {
+  return (digitalRead(pinoSensor) == 1);
 }
